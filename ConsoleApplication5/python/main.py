d00edef69169544281d536863db1c7bc1dca49f7
FunnyWelder <slavakuzm@mail.ru>
2022-01-13 02:06:29 +1000

    сделал вывод нескольких графиков

--- ConsoleApplication5/python/main.py
+++ ConsoleApplication5/python/main.py
@@ -10,21 +10,38 @@ class end_error(Exception):
 
 
 def histoogramm(data):
-    plt.subplot(3, 1, 1)
+    plt.subplot(2, 1, 1)
     plt.plot(data["time_ms"], linewidth=2.0)
-    plt.xlabel("Время")
-    plt.ylabel("Число ядер")
+    plt.ylabel("Время")
     plt.grid(True)
     plt.title(data["name"])
 
-    plt.subplot(3, 1, 3)
+    plt.subplot(2, 1, 2)
     plt.plot(data["speed"], linewidth=2.0)
-    plt.xlabel("Скорость")
-    plt.ylabel("Число ядер")
+    plt.xlabel("Число ядер")
+    plt.ylabel("Ускорение")
     plt.grid(True)
+    
+    plt.subplots_adjust(hspace = .05)
 
     plt.show()
 
 if __name__ == "__main__":
-    data1 = db.read("1.json", "graphics")
+    data1 = db.read("integrate_aligned.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_cpp.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_cpp_mtx.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_cpp_reduction.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_crit.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_false_sharing.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_omp_for.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_reduce.json", "graphics")
+    histoogramm(data1)
+    data1 = db.read("integrate_reduction.json", "graphics")
     histoogramm(data1)
